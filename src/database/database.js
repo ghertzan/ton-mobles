34970e68101313e95d2cde9788585971572e831e
gabriel hertzan <gabriel.hertzan@gmail.com>
2025-06-28 10:46:27 -0300

    creación de database

---
+++ src/database/database.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWD9Vtj01Hxtiw5pU8GNOdtXoXm-RkqEY",
+  authDomain: "coder-ton-mobles.firebaseapp.com",
+  projectId: "coder-ton-mobles",
+  storageBucket: "coder-ton-mobles.firebasestorage.app",
+  messagingSenderId: "511510089417",
+  appId: "1:511510089417:web:a1c3e9f2154a1c22338718",
+  measurementId: "G-DBBRMRXJX5",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
