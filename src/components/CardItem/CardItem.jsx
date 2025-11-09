50dfd8751851bfb8694bb47c14c6fffc89d38c7a
Gabriel Hertzan <Gabriel.Hertzan@gmail.com>
2025-11-09 10:54:36 -0300

    conección con back basico

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,24 +1,22 @@
 import { NavLink } from "react-router";
 
 export default function CardItem({ item }) {
-  return (
-    <div
-      key={item.id}
-      className="card m-1 p-0 col-1 align-items-center border border-bg-danger-subtle"
-      style={{ width: "18rem" }}
-    >
-      <img
-        src={item.thumbnail}
-        className="card-img-top"
-        alt={item.description}
-      ></img>
-      <div className="card-body">
-        <h5 className="card-title"> {item.title}</h5>
-        <h5 className="card-title text-muted"> $ {item.price}</h5>
-      </div>
-      <NavLink to={`/item/${item.id}`} className="btn btn-primary w-50 mb-1">
-        Ver más
-      </NavLink>
-    </div>
-  );
+	return (
+		<div
+			key={item.id}
+			className="card m-1 p-0 col-1 align-items-center border border-bg-danger-subtle"
+			style={{ width: "18rem" }}>
+			<img
+				src={item.thumbnails}
+				className="card-img-top"
+				alt={item.description}></img>
+			<div className="card-body">
+				<h5 className="card-title"> {item.title}</h5>
+				<h5 className="card-title text-muted"> $ {item.price}</h5>
+			</div>
+			<NavLink to={`/item/${item.id}`} className="btn btn-primary w-50 mb-1">
+				Ver más
+			</NavLink>
+		</div>
+	);
 }
--- src/database/db.js
+++ src/database/db.js
@@ -1,35 +1,22 @@
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  query,
-  where,
-  doc,
-  getDoc,
-} from "firebase/firestore";
-
-import { app } from "./config";
-
-const db = getFirestore(app);
-
+const URL = "http://localhost:8080";
 /**
  *
  * @returns {Array} con todos los articulos en la base
  */
 export const getAllProducts = async () => {
-  try {
-    const documents = await getDocs(collection(db, "products"));
+	try {
+		const result = await fetch(`${URL}/api/products/`);
+		const data = await result.json();
 
-    const products = [];
+		const products = [];
 
-    documents.forEach((doc) => {
-      products.push({ ...doc.data(), id: doc.id });
-    });
-
-    return products;
-  } catch (error) {
-    console.error(error);
-  }
+		data.payload.forEach((prod) => {
+			products.push({ id: prod._id, ...prod });
+		});
+		return products;
+	} catch (error) {
+		console.error(error);
+	}
 };
 /**
  *
@@ -37,14 +24,14 @@ export const getAllProducts = async () => {
  * @returns {object} objeto que contiene la información del producto y su identificador
  */
 export const getProduct = async (id) => {
-  const docRef = doc(db, "products", id);
-  const docSnap = await getDoc(docRef);
+	const docRef = doc(db, "products", id);
+	const docSnap = await getDoc(docRef);
 
-  if (docSnap.exists) {
-    return { ...docSnap.data(), id: docRef.id };
-  } else {
-    return null;
-  }
+	if (docSnap.exists) {
+		return { ...docSnap.data(), id: docRef.id };
+	} else {
+		return null;
+	}
 };
 
 /**
@@ -53,14 +40,14 @@ export const getProduct = async (id) => {
  * @returns {Array} con los productos en la base según la categoria
  */
 export const getProductsByCategory = async (category) => {
-  const products = [];
-  const q = query(
-    collection(db, "products"),
-    where("category", "==", category)
-  );
-  const docs = await getDocs(q);
-  docs.forEach((doc) => {
-    products.push({ ...doc.data(), id: doc.id });
-  });
-  return products;
+	const products = [];
+	const q = query(
+		collection(db, "products"),
+		where("category", "==", category)
+	);
+	const docs = await getDocs(q);
+	docs.forEach((doc) => {
+		products.push({ ...doc.data(), id: doc.id });
+	});
+	return products;
 };
